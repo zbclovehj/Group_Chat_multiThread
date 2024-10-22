f7c19819438eba15fde8280bb5dd9f0d2634aad6
zbclovehj <zbc8301@gmail.com>
2024-10-22 19:18:46 +0800

    使用Tcp实现的多客户端与服务器通信的群聊功能，使用多线程以及互斥来实现并发

---
+++ client/client.cpp
@@ -0,0 +1,93 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<windows.h>
+#include<graphics.h>//easyX
+#include <process.h>
+#pragma comment(lib, "ws2_32.lib")
+
+SOCKET clientSocket;
+HWND hwnd;
+//记录信息数量
+int count = 0;
+//从服务器接受信息并输出到每个客户的群聊里
+void recvive() {
+
+	char recvBuff[1024];
+	int r;
+	while (1) {
+		r = recv(clientSocket, recvBuff, 1023, NULL);
+		if (r > 0) {
+			recvBuff[r] = 0;
+			//将信息输出到界面上，一条信息二十像素间距
+		
+			outtextxy(0, count * 20, recvBuff);
+			count++;
+		}
+	}
+
+}
+int main() {
+	//初始化界面
+	hwnd = initgraph(400, 400, SHOWCONSOLE);
+	//1 请求协议版本
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	//判断版本是否正确
+	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+		printf("请求协议版本失败！\n");
+		return -1;
+	}
+	//2 创建客户端的socket
+	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (SOCKET_ERROR == clientSocket)
+	{
+		printf("创建socket失败！\n");
+		WSACleanup();
+		return -2;
+	}
+	printf("创建socket成功！");
+	//3 获取服务器协议地址族
+	SOCKADDR_IN addr_server = { 0 };
+	addr_server.sin_family = AF_INET;
+	addr_server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	/*
+		‌小端模式（Little - endian）‌是指数据的高字节保存在内存的高地址中，
+		而数据的低字节保存在内存的低地址中。
+		这种存储模式将地址的高低和数据位权值有效地结合起来，
+		使得低地址部分权值低，高地址部分权值高。‌
+
+	大端模式（Big - endian）‌是指数据的高字节保存在内存的低地址中，
+		而数据的低字节保存在内存的高地址中。这种存储模式类似于将数据当作字符串顺序处理，
+		地址由小向大增加，而数据从高位往低位放。
+	*/
+	//os内核和其他程序会占用掉一些端口,所以不能乱用
+	addr_server.sin_port = htons(10086);
+	// 4 connect连接服务器
+	int r = connect(clientSocket, (sockaddr*)&addr_server, sizeof(addr_server));
+	if (-1 == r)
+	{
+		printf("连接服务器失败！");
+		closesocket(clientSocket);
+		WSACleanup();
+		return -3;
+	}
+	printf("连接服务器成功！");
+	// 5 通信
+
+	char buff[1024];
+
+	unsigned int threadId;
+	//创建客户端的线程，在一个进程当中创建一个线程去处理另外一部分代码逻辑
+	_beginthreadex(NULL, 0, (_beginthreadex_proc_type)recvive, NULL, 0, &threadId);
+	//向服务器发送聊天信息
+	while (1) {
+		memset(buff, 0, 1024);
+		printf("你想说啥：\n");
+		scanf("%s", buff);
+		printf("你说：%s\n", buff);
+		send(clientSocket, buff, strlen(buff), NULL);
+		
+	}
+	return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,134 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+#include <winsock2.h>
+#include <process.h>
+#include<windows.h>
+#pragma comment(lib, "ws2_32.lib")
+SOCKADDR_IN addr_c = { 0 };
+int len = sizeof(addr_c);
+SOCKET clientSocket[1024];
+int count = 0;
+HANDLE mutex;
+
+void connecting(void* param) {
+    int idx = *((int*)param);
+    char buff[1024];
+    int r;
+    while (1) {
+
+        r = recv(clientSocket[idx], buff, 1023, NULL);
+        if (r > 0) {
+            buff[r] = 0;
+            printf("%d:%s\n", idx, buff);
+
+            // 广播数据，实现多人群聊，向所有用户转发信息,
+            // 加锁，防止每个用户的信息顺序不一致
+            WaitForSingleObject(mutex, INFINITE);
+
+            for (int i = 0; i < count; ++i) {
+                send(clientSocket[i], buff, strlen(buff), NULL);
+            }
+
+            // 释放锁
+            ReleaseMutex(mutex);
+        }
+    }
+}
+
+int main() {
+    //1 请求协议版本
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    //判断版本是否正确
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+        printf("请求协议版本失败！\n");
+        return -1;
+    }
+    //2 创建服务器的socket
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (SOCKET_ERROR == serverSocket)
+    {
+        printf("创建socket失败！\n");
+        WSACleanup();
+        return -2;
+    }
+    printf("创建socket成功！");
+    //3 创建协议地址族
+    SOCKADDR_IN addr_server = { 0 };
+    addr_server.sin_family = AF_INET;
+    addr_server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    /*
+    ‌小端模式（Little - endian）‌是指数据的高字节保存在内存的高地址中，
+    而数据的低字节保存在内存的低地址中。
+    这种存储模式将地址的高低和数据位权值有效地结合起来，
+    使得低地址部分权值低，高地址部分权值高。‌
+    大端模式（Big - endian）‌是指数据的高字节保存在内存的低地址中，
+	而数据的低字节保存在内存的高地址中。这种存储模式类似于将数据当作字符串顺序处理，
+	地址由小向大增加，而数据从高位往低位放。
+*/
+//os内核和其他程序会占用掉一些端口,所以不能乱用
+    addr_server.sin_port = htons(10086);
+    // 4 将socket与服务器地址端口绑定
+    int r = bind(serverSocket, (sockaddr*)&addr_server, sizeof(addr_server));
+    if (-1 == r)
+    {
+        printf("bind失败！");
+        closesocket(serverSocket);
+        WSACleanup();
+        return -3;
+    }
+    // 5 监听
+    r = listen(serverSocket, 10);
+    if (-1 == r)
+    {
+        printf("listen失败！");
+        closesocket(serverSocket);
+        WSACleanup();
+        return -3;
+    }
+    printf("listen成功！");
+    //6 等待客户端连接 阻塞等待 
+
+    // 接受客户端连接并生成一个与相应客户端通信的socket，并获取客户端的端口与地址
+
+    // 创建互斥锁
+    mutex = CreateMutex(NULL, FALSE, NULL);
+
+    // 多线程处理客户端连接
+    while (1) {
+        clientSocket[count] = accept(serverSocket, (sockaddr*)&addr_c, &len);
+        if (clientSocket[count] == SOCKET_ERROR) {
+            printf("服务器宕机了！\n");
+            //清除协议信息
+            WSACleanup();
+            return -2;
+        }
+        printf("有客户端连接！地址为：%s\n", inet_ntoa(addr_c.sin_addr));
+
+        // 创建与客户端通信的线程
+    
+        unsigned int threadId;
+        int* idx = new int(count); // 为每个线程传递不同的索引
+
+        // 安全复制索引
+        int* safe_idx = new int(*idx);
+
+        _beginthreadex(NULL, 0, (_beginthreadex_proc_type)connecting, safe_idx, 0, &threadId);
+
+        count++;
+    }
+
+    // 关闭连接、释放资源
+    for (int i = 0; i < count; i++) {
+        closesocket(clientSocket[i]);
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+    // 关闭互斥锁
+    CloseHandle(mutex);
+
+    return 0;
+}
